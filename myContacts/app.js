8196d5336a327933b8ccb95d876b614992d39ec0
김민기 <loopang5954@gmail.com>
2025-01-21 16:48:43 +0900

    몽고고

--- myContacts/app.js
+++ myContacts/app.js
@@ -1,35 +1,17 @@
 const express = require("express");
+const dbConnect = require("./config/dbConnect")
 const app = express();
+
+dbConnect();
 
 app.get("/", (req, res) => {
   res.send("Hello Node");
 });
 
-//연락처 가져오기
-app.get("/contacts", (req, res) => {
-  res.send("contacts");
-});
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-//새 연락처 추가하기
-app.post("/contacts", (req, res) => {
-  res.send("creact contacts");
-});
-
-//연락처 1개 가져오기
-app.get("/contacts/:id", (req, res) => {
-  res.send(`View Contact for ID : ${req.params.id}`);
-});
-
-
-//연락처 수정하기기
-app.put("/contacts/:id", (req, res) => {
-  res.send(`update contact  for id : ${req.params.id}`);
-});
-
-//연락처 삭제하기
-app.delete("/contacts/:id", (req, res) => {
-  res.send(`Delete Contact for Id : ${req.params.id}`);
-});
+app.use("/contacts", require("./routes/contactRoutes"));
 
 app.listen(3000, () => {
   console.log("서버 실행 중");
