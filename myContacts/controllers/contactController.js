fa266811696e4028836a93d0ced4fdf27888e3bd
김민기 <loopang5954@gmail.com>
2025-02-03 12:51:37 +0900

    1

--- myContacts/controllers/contactController.js
+++ myContacts/controllers/contactController.js
@@ -12,7 +12,26 @@ const createContact = asyncHandler((req, res) => {
   if (!name || !email || !phone) {
     return res.send("필수 값이 입력되지 않았습니다.");
   }
-  res.send("create contacts122222");
+  res.send("create contacts");
 });
 
-module.exports = {getAllContacts, createContact};
+const getContact = asyncHandler((req, res) => {
+  res.send(`Update Contact for ID: ${req.params.id}`);
+});
+
+const updateContact = asyncHandler(async (req, res) => {
+  // 연락처 수정하기
+  res.status(200).send(`Update Contact for ID: ${req.params.id}`);
+});
+
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).send(`Delete Contact for ID: ${req.params.id}`);
+});
+
+module.exports = {
+  getAllContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
--- myContacts/routes/contactRoutes.js
+++ myContacts/routes/contactRoutes.js
@@ -1,31 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const {getAllContacts, createContact} = require("../controllers/contactController")
+const {
+  getAllContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController");
 
-
-router
-  .route("/")
-  .get(getAllContacts)
-  .post(createContact)
-  // .post((req, res) => {
-  //   console.log(req.body);
-  //   const {name, email, phone} = req.body;
-  //   if(!name || !email || !phone){
-  //     return res.send("필수 값이 입력되지 않았습니다.")
-  //   }
-  //   res.send("create contacts");
-  // });
+router.route("/").get(getAllContacts).post(createContact);
+// .post((req, res) => {
+//   console.log(req.body);
+//   const {name, email, phone} = req.body;
+//   if(!name || !email || !phone){
+//     return res.send("필수 값이 입력되지 않았습니다.")
+//   }
+//   res.send("create contacts");
+// });
 
 router
   .route("/:id")
-  .get((req, res) => {
-    res.send(`View Contact for ID : ${req.params.id}`);
-  })
-  .put((req, res) => {
-    res.send(`Update Contact for id : ${req.params.id}`);
-  })
-  .delete((req, res) => {
-    res.send(`Delete Contact for ID : ${req.params.id}`);
-  });
+  .get(getContact)
+  .put(updateContact)
+  .delete(deleteContact);
 
-  module.exports = router
+module.exports = router;
