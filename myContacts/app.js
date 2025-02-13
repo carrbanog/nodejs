c62af4b0aa5df9d05832bfa916ac3ca52ec78005
김민기 <loopang5954@gmail.com>
2025-02-13 15:13:43 +0900

    login test

--- myContacts/app.js
+++ myContacts/app.js
@@ -7,24 +7,20 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.use(express.static(path.join(__dirname, "assets")));
+// app.use(express.static(path.join(__dirname, "assets")));
 app.use(express.static("./public"))
 // app.set("views", path.join(__dirname, "views"));
 app.use(methodOverride("_method"));
 dbConnect();
 
-// app.get("/", (req, res) => {
-//   // res.send("Hello Node");
-//   console.log(dbConnect)
-//   res.render("home.ejs")
-// });
+
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", require("./routes/loginRoutes"));
 app.use("/contacts", require("./routes/contactRoutes"));
+app.use("/", require("./routes/loginRoutes"));
 
 app.listen(3000, () => {
   console.log("서버 실행 중");
--- myContacts/controllers/loginController.js
+++ myContacts/controllers/loginController.js
@@ -4,4 +4,18 @@ const getLogin = ((req, res) => {
   res.render("home.ejs")
 })
 
-module.exports = getLogin
+
+//login userr
+//post
+const loginUser = asyncHandler(async(req, res) => {
+  const {username, password} = req.body;
+  if(username === "admin" && password === "1234"){
+    // console.log(username, password);
+    res.send("login success");
+  }else{
+    res.send("login failed");
+    // console.log(username, password);
+  }
+})
+
+module.exports = {getLogin, loginUser}
