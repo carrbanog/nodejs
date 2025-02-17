dc5669255db738d37e96054c48cf8d9bb56b39c8
김민기 <loopang5954@gmail.com>
2025-02-17 13:53:56 +0900

    token

--- myContacts/controllers/loginController.js
+++ myContacts/controllers/loginController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+require("dotenv").config();
+const jwtSecret = process.env.JWT_SECRET;
+const jwt = require("jsonwebtoken");
 
 const getLogin = (req, res) => {
   res.render("home.ejs");
@@ -10,13 +13,26 @@ const getLogin = (req, res) => {
 //post
 const loginUser = asyncHandler(async (req, res) => {
   const { username, password } = req.body;
-  if (username === "admin" && password === "1234") {
-    // console.log(username, password);
-    res.send("login success");
-  } else {
-    res.send("login failed");
-    // console.log(username, password);
+  // if (username === "admin" && password === "1234") {
+  //   // console.log(username, password);
+  //   res.send("login success");
+  // } else {
+  //   res.send("login failed");
+  //   // console.log(username, password);
+  // }
+  const user = await User.findOne({username});
+  if(!user){
+    return res.json({message: "일치하는 사용자가 없습니다."});
   }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch){
+    return res.json({message: "비밀번호가 맞지 않습니다."});
+  }
+
+  const token = jwt.sign({id: user._id}, jwtSecret);
+  res.cookie("token", token,  {httpOnly: true});
+  console.log(user, isMatch, token);
+  res.redirect("/contacts");
 });
 
 // Register page
